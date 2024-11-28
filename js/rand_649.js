1dda8441b3570f465366592a89b5fbca5e2ee786
奶喵 <75056749+Meow-OuO@users.noreply.github.com>
2024-11-29 04:03:31 +0800

    Update rand_649.js

--- js/rand_649.js
+++ js/rand_649.js
@@ -13,12 +13,5 @@ function roll() {
     }
   }
 
-  let mainNumbers = arr.slice(0, 6);
-  mainNumbers.sort(function(a, b) {
-    return a - b;
-  });
-
-  let specialNumber = arr[6];
-
-  document.getElementById("lotto649").innerHTML = '開獎號碼:' + "&nbsp;" + mainNumbers.join(",") + "&emsp;" + '特別號:' + specialNumber;
+document.getElementById("lotto649").innerHTML = '開獎號碼:' + "&nbsp;" + arr.slice(0, 6).sort(function(a, b){ return a - b; }).join(",") + "&emsp;" + '特別號:' + arr[6];
 }
