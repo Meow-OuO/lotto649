47edf652efcfce52f54fd5e3eddaaea7aee75cf1
奶喵 <75056749+Meow-OuO@users.noreply.github.com>
2024-11-29 01:30:39 +0800

    Update rand_649.js

--- js/rand_649.js
+++ js/rand_649.js
@@ -1,17 +1,33 @@
 function rand(max) {
-  return Math.floor(Math.random() * max)+1;
+  return Math.floor(Math.random() * max) + 1;
 }
 
 function roll() {
-	let num = 49;
-	let arr = [];
-	let random = rand(num);
-	
-	for (i = 1; i <= 6; i++) {
-		arr.push(random);
-		while (arr.includes(random)) {
-			random = rand(num);
-		}
-	}
-	document.getElementById("lotto649").innerHTML='開獎號碼:'+"&nbsp;"+arr.sort(function(a, b){return a-b});
+  let num = 49; // 生成 1 到 49 的隨機數字
+  let arr = [];
+
+  // 生成 6 個不重複的正常號碼
+  while (arr.length < 6) {
+    let random = rand(num);
+    if (!arr.includes(random)) {
+      arr.push(random);
+    }
+  }
+
+  // 生成特別號，並確保不與正常號碼重複
+  let specialNumber;
+  do {
+    specialNumber = rand(num);
+  } while (arr.includes(specialNumber));
+
+  // 將正常號碼排序
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+
+  // 將正常號碼和特別號結合
+  arr.push(specialNumber);
+
+  // 顯示結果
+  document.getElementById("lotto649").innerHTML = '開獎號碼: ' + arr.slice(0, 6).join('&nbsp;') + ' &nbsp; 特別號: ' + arr[6];
 }
